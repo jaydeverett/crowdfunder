f1ef3bbb512de0becc3168b2fc5eb5439a6c4979
Jay Deverett <jayjdeverett@gmail.com>
2017-08-01 18:31:59 -0400

    resolve first four test failures

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,11 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner_back_own_project
+
+  def owner_back_own_project
+    if user == project.owner
+      errors.add :user, "Cannot back own project"
+    end
+  end
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,6 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
+
+ validates :description, presence: true
+ validates :dollar_amount, presence: true
 end
