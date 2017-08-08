9762e6c3f0f9fb18f079c9761d280420513ee4e2
Jay Deverett <jayjdeverett@gmail.com>
2017-08-08 15:55:28 -0400

    backer can leave comment on project they're backing test passed

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ group :development, :test do
   gem 'faker'
   gem 'factory_girl_rails'
   gem 'capybara'
+  gem 'launchy'
 end
 
 group :development do
--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -5,8 +5,10 @@ class ProjectsTest < ActionDispatch::IntegrationTest
   test "backer can leave a comment on a project they've backed" do
     project = Project.create(title: "Cool Project", description: "A cool project", goal: 25000, start_date: Date.today, end_date: Date.today+30)
     visit "projects/#{project.id}"
-    # fill_in 
-
-
+    fill_in :comment_comment, with: "nice project!"
+    click_on 'Create Comment'
+    save_and_open_page
+    assert_text "nice project!"
+    click_on 'Create Comment'
   end
 end
