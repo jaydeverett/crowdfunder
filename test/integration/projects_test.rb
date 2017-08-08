63f993df5693e86bd3a866511285324b2c2d312a
Jay Deverett <jayjdeverett@gmail.com>
2017-08-08 15:15:15 -0400

    green tests

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -3,8 +3,10 @@ require 'test_helper'
 class ProjectsTest < ActionDispatch::IntegrationTest
 
   test "backer can leave a comment on a project they've backed" do
-    Project.create(title: "Cool Project", description: "A cool project", goal: 25000, start_date: Date.today, end_date: Date.today+30)
-    visit 'projects'
+    project = Project.create(title: "Cool Project", description: "A cool project", goal: 25000, start_date: Date.today, end_date: Date.today+30)
+    visit "projects/#{project.id}"
+    # fill_in 
+
 
   end
 end
